395296be58fcfe94a65cba0603ef65753de3bdac
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-02-18 15:56:02 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/eliasbobadilla/Ultimate-Wallpaper-Changer.git ~/.local/share/gnome-shell/extensions/ultimate-wallpaper-changer@eliasbobadilla
+cd ~/.local/share/gnome-shell/extensions/ultimate-wallpaper-changer@eliasbobadilla
+glib-compile-schemas ./schemas/
